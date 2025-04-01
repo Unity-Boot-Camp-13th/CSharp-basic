a88908dd3dcd3ac084e53facc26bb4c196e53323
todays-sun-day <178suna@gmail.com>
2025-04-01 12:52:46 +0900

    동적 배열, 제네릭, 리스트 이용

---
+++ CSharpBasic/CollectionsSample/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.ComponentModel.DataAnnotations;
+
+namespace CollectionsSample
+{
+    internal class Program
+    {
+        Action<int> action; // 파라미터 0~ 16개 까지, 반환형 void 인 함수 참조 체이닝을 위한 C# 기본 대리자
+        Func<int, bool> func; // 파라미터 0~ 16개 까지, 반환형 제네릭타입인 함수 참조 체이닝을 위한 C# 기본 대리자
+        Predicate<int> predicate; // 파라미터 1개, 반환형 bool 인 함수 참조체이닝을 위한 C# 기본 대리자 (보통 조건 확인용)
+
+        static void Main(string[] args)
+        {
+            DynamicArray dynamicArray = new DynamicArray(4);
+            dynamicArray.Add(1);
+            dynamicArray.Add(4);
+            dynamicArray.Add(2);
+            dynamicArray.Add(5);
+            dynamicArray.Add(3);
+            dynamicArray.RemoveAt(4);
+            dynamicArray.FindLastIndex(x => (int)x > 3);
+
+            Predicate<object> match = x => (int)x > 3;
+
+            IEnumerator enumerator = dynamicArray.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current);
+            }
+
+            enumerator.Reset();
+
+            for (; enumerator.MoveNext(); )
+            {
+                Console.WriteLine(enumerator.Current);
+            }
+
+            DynamicArray<int> numbers1 = new DynamicArray<int>(4);
+            numbers1.Add(1);
+            numbers1.Add(2);
+
+            DynamicArray<int> numbers2 = new DynamicArray<int>(4);
+            numbers2.Add(4);
+            numbers2.Add(6);
+            numbers2.Add(1);
+
+            DynamicArray<string> rewards = new DynamicArray<string>(4);
+            rewards.Add("Hp Potion");
+            rewards.Add("Mp Potion");
+            rewards.Add("Basic sword");
+
+            TreasureChest treasureChest = new TreasureChest(rewards);
+            Console.WriteLine($"보상 받음 : {treasureChest.GetRandomReward()}");
+
+            for (int i = 0; i < rewards.Count; i++)
+            {
+                Console.Write($"{rewards[i]}, ");
+            }
+
+            IEnumerator<int> eNumbers = numbers1.GetEnumerator(); // 책 읽어주는 사람
+
+            // 책 읽어주는 사람에게 현재 페이지 읽고 다음장 넘겨주세요
+            // 현재 페이지 읽는데 성공했으면 true. 아니면 false.
+            while (eNumbers.MoveNext())
+            {
+                Console.WriteLine(eNumbers.Current); // 책 읽어주는 사람에게 현재 페이지 나에게 넘겨주세요
+            }
+
+            eNumbers.Dispose(); // 이 책을 읽기 위해서 사용했던 관리되지 않는 리소스를 해제
+
+            IEnumerator<int> e1 = numbers1.GetEnumerator();
+            IEnumerator<int> e2 = numbers2.GetEnumerator();
+
+            while (e1.MoveNext() && e2.MoveNext())
+            {
+
+            }
+
+            // foreach 구문
+            // IEnumerable 에 대해서 GetEnumerator() 호출하여
+            // Enumeration 을 수행하는 구문
+            foreach (int item in numbers1)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ CSharpBasic/CollectionsSample/TreasureChest.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace CollectionsSample
+{
+    class TreasureChest
+    {
+        public TreasureChest(DynamicArray<string> rewards)
+        {
+            rewards.Add("Basic bow");
+            rewards.Remove(x => x.Contains("sword"));
+            _rewards = new string[rewards.Count];
+
+            for (int i = 0; i < rewards.Count; i++)
+            {
+                _rewards[i] = rewards[i];
+            }
+        }
+
+        string[] _rewards;
+
+        public string GetRandomReward()
+        {
+            Random random = new Random();
+            int randomIndex = random.Next(0, _rewards.Length);
+            return _rewards[randomIndex];
+        }
+    }
+}
